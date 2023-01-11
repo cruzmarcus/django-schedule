5cad70be91d724ff1b7663dfdf1faf15ee5d0440
Marcus Cruz <marcus.v.s.c13@gmail.com>
2023-01-10 22:36:43 -0300

    admin and models

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from contacts.models import Category, Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "last_name",
+        "phone",
+        "email",
+        "created_date",
+        "category",
+    )
+    list_display_links = ("id", "name", "last_name")
+    list_filter = ("name", "last_name")
+    list_per_page = 10
+    search_fields = ("name", "last_name")
+
+
+admin.site.register(Category)
+admin.site.register(Contact, ContactAdmin)
